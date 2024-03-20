e89b5b3c54ec8368fdb15c15b0ba98ee5fe720d6
arun <crcravi66@gmail.com>
2024-03-20 13:30:43 +0530

    finshed for form event page

--- formevent.js
+++ formevent.js
@@ -1,13 +1,16 @@
 const form = document.querySelector("#form");
 const input = document.querySelector('#formInput');
 const submited = document.querySelector('#submitButton');
-const li =document.querySelector("#ul-container");
+const li = document.querySelector("#ul-container");
 
 form.addEventListener("submit", function (e) {
     e.preventDefault();
     const newList = document.createElement('li');
     const listvalues = input.value ;
     newList.innerText = listvalues ;
-    li.append(newList)
+    if (input.value){   
+        li.append(newList)
+    }
     input.value = "";
+    
 });
